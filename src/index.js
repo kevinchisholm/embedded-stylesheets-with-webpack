dc465c50580827b1226e692e1c4ababa4347c76b
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-02-03 22:02:14 -0500

    Add core files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+var $ = require('jquery');
+require('./style-2.css');
+require('./style-3.css');
+
+$('body').append('<h1>Hello Webpack!</h1>');
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: 'build',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader'
+			}
+	 ]
+	}
+};
